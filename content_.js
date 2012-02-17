251ac941cbb3e47d931b4e26b7b5d00dd8aee381
Alan Gutierrez <alan@prettyrobots.com>
2012-02-17 01:02:20 -0500

    Removed backup file.

--- content_.js
+++
@@ -1,7 +0,0 @@
-chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
-  var script = document.createElement("script");
-  script.setAttribute("type", "text/javascript");
-  script.setAttribute("src", request.uri);
-  document.getElementsByTagName("head")[0].appendChild(script);
-  sendResponse({});
-});
