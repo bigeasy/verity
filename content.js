524b4dc9c411838abad564992872e83c86abd8b3
Alan Gutierrez <alan@prettyrobots.com>
2012-02-19 20:27:37 -0500

    Remove Chrome specific bits of common extension.

--- content.js
+++
@@ -1,7 +0,0 @@
-chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
-  var script = document.createElement("script");
-  script.setAttribute("type", "text/javascript");
-  script.appendChild(document.createTextNode(request));
-  document.getElementsByTagName("head")[0].appendChild(script);
-  sendResponse({});
-});
