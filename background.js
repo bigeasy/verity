4dbc7d6c22c61a2993a375ab55f8205e02827293
Alan Gutierrez <alan@prettyrobots.com>
2012-02-21 03:33:00 -0500

    Local storage and evaluation of expected tests.

--- background.js
+++ background.js
@@ -1,7 +1,30 @@
 chrome.webNavigation.onCompleted.addListener(function (details) {
+  var settings = {};
+  var injections = JSON.parse(localStorage["injections"] || "[]");
   if (details.frameId == 0) {
-    shouldTest(XHRRequest, details.url, function (source) {
-      chrome.tabs.sendRequest(details.tabId, source, function () {});
+    shouldTest({
+      url: details.url,
+      boilerplate: boilerplate,
+      removeInjection: function (target, source) {
+        injections = injections.filter(function (e) { e.target != target });
+        localStorage["injections"] = JSON.stringify(injections);
+      },
+      addInjection: function (target, source) {
+        injections.push({ target: target, source: source });
+        localStorage["injections"] = JSON.stringify(injections);
+      },
+      injections: injections.concat([
+        { target: /jekyllrb.com/.toString()
+        , source: "http://dossier:8048/verity/user/landing/index.js"
+        }
+      ]),
+      createRequest: createXHRRequest,
+      settings: settings,
+      injector: function (source) {
+        if (source) {
+          chrome.tabs.sendRequest(details.tabId, source, function () {});
+        }
+      }
     });
   }
 });
