786f3bec40b8188e79ad0bacdfd7e33f2297e8ab
Alan Gutierrez <alan@prettyrobots.com>
2012-02-21 03:53:12 -0500

    Local storage and evaluation of expected tests.

--- verity.safariextension/background.js
+++ verity.safariextension/background.js
@@ -1,6 +1,27 @@
 function listener (event) {
-  shouldTest(XHRRequest, event.message, function (source) {
-    event.target.page.dispatchMessage("source", source);
+  var injections = JSON.parse(localStorage["injections"] || "[]");
+  shouldTest({
+    url: event.message,
+    boilerplate: boilerplate,
+    removeInjection: function (target) {
+      injections = injections.filter(function (e) { e.target != target });
+      localStorage["injections"] = JSON.stringify(injections);
+    },
+    addInjection: function (target, source) {
+      injections.push({ target: target, source: source });
+      localStorage["injections"] = JSON.stringify(injections);
+    },
+    injections: injections.concat([
+      { target: /jekyllrb.com/.toString()
+      , source: "http://dossier:8048/verity/user/landing/index.js"
+      }
+    ]),
+    createRequest: createXHRRequest,
+    injector: function (source) {
+      if (source) {
+        event.target.page.dispatchMessage("source", source);
+      }
+    }
   });
 }
 safari.application.addEventListener("message", listener, false);
