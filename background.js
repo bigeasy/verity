b67af5cf24d3a6ed5fd7c8f361fc2d953ed2d761
Alan Gutierrez <alan@prettyrobots.com>
2012-02-21 04:40:24 -0500

    Add XHR factory for sake of IE.

--- background.js
+++ background.js
@@ -1,11 +1,10 @@
 chrome.webNavigation.onCompleted.addListener(function (details) {
-  var settings = {};
   var injections = JSON.parse(localStorage["injections"] || "[]");
   if (details.frameId == 0) {
     shouldTest({
       url: details.url,
       boilerplate: boilerplate,
-      removeInjection: function (target, source) {
+      removeInjection: function (target) {
         injections = injections.filter(function (e) { e.target != target });
         localStorage["injections"] = JSON.stringify(injections);
       },
@@ -18,8 +17,7 @@ chrome.webNavigation.onCompleted.addListener(function (details) {
         , source: "http://dossier:8048/verity/user/landing/index.js"
         }
       ]),
-      createRequest: createXHRRequest,
-      settings: settings,
+      createRequest: createXHRRequest(function () { return new XMLHttpRequest() }),
       injector: function (source) {
         if (source) {
           chrome.tabs.sendRequest(details.tabId, source, function () {});
